660e6b4b528f84d51b47f3e8fd05fe280e42affb
Guillermo Fernández <gafernandez2525@gmail.com>
2024-06-07 17:27:04 +0200

    Use .env file for environment variables
    Save database connection param there

--- core/Database.php
+++ core/Database.php
@@ -4,44 +4,30 @@ declare(strict_types=1);
 
 namespace Core;
 
+use Dotenv\Dotenv;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 class Database
 {
-    private const PARAMS = [
-        'driver' => 'mysql',
-        'host' => 'duacode-db',
-        'database' => 'duacode',
-        'username' => 'root',
-        'password' => '1234',
-//        'charset' => 'utf8',
-//        'collation' => 'utf8mb4_unicode_ci',
-//        'prefix' => '',
-    ];
-
     public static function setDatabaseConnection(): void
     {
+        $dotenv = Dotenv::createImmutable(dirname(__DIR__, 1));
+        $dotenv->load();
+
+        $params = [
+            'driver' => $_ENV['DB_CONNECTION'],
+            'host' => $_ENV['DB_HOST'],
+            'database' => $_ENV['DB_DATABASE'],
+            'username' => $_ENV['DB_USERNAME'],
+            'password' => $_ENV['DB_PASSWORD'],
+        ];
+
         $capsule = new Capsule();
 
-        $capsule->addConnection(self::PARAMS);
+        $capsule->addConnection($params);
 
         $capsule->setAsGlobal();
 
         $capsule->bootEloquent();
     }
-
-//    private static self $instance;
-//
-//    private function __construct()
-//    {
-//        (new Capsule())->addConnection(self::PARAMS);
-//    }
-
-//    public static function getInstance(): self
-//    {
-//        if (!self::$instance) {
-//            self::$instance = new self();
-//        }
-//        return self::$instance;
-//    }
 }
