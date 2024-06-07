6f5c1d48433653b23fd9a05411cb1f4729c7fffe
Guillermo Fernández <gafernandez2525@gmail.com>
2024-06-07 22:29:31 +0200

    Fix sql file

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -2,61 +2,67 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- Base de datos: 'sport_teams'
+-- Base de datos: `sport_teams`
 --
-CREATE DATABASE IF NOT EXISTS 'sport_teams' DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE 'sport_teams';
+CREATE DATABASE IF NOT EXISTS `sport_teams` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `sport_teams`;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla 'players'
+-- Estructura de tabla para la tabla `players`
 --
 
-CREATE TABLE 'players' (
-                           'id' int(20) NOT NULL,
-                           'first_name' varchar(255) NOT NULL,
-                           'last_name' varchar(255) NOT NULL,
-                           'team_id' int(11) NOT NULL,
-                           'number' tinyint(3) UNSIGNED NOT NULL,
-                           'birth_date' date DEFAULT NULL
+CREATE TABLE `players` (
+                           `id` int(20) NOT NULL,
+                           `first_name` varchar(255) NOT NULL,
+                           `last_name` varchar(255) NOT NULL,
+                           `team_id` int(11) NOT NULL,
+                           `number` tinyint(3) UNSIGNED NOT NULL,
+                           `birth_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla 'sports'
+-- Estructura de tabla para la tabla `sports`
 --
 
-CREATE TABLE 'sports' (
-                          'id' int(11) NOT NULL,
-                          'name' varchar(255) NOT NULL
+CREATE TABLE `sports` (
+                          `id` int(11) NOT NULL,
+                          `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Volcado de datos para la tabla 'sports'
+-- Volcado de datos para la tabla `sports`
 --
 
-INSERT INTO 'sports' ('id', 'name') VALUES
+INSERT INTO `sports` (`id`, `name`) VALUES
                                         (1, 'fútbol'),
                                         (2, 'baloncesto'),
-                                        (3, 'balonmano'),
-                                        (4, 'rugby');
+                                        (3, 'natación'),
+                                        (4, 'tenis');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla 'teams'
+-- Estructura de tabla para la tabla `teams`
 --
 
-CREATE TABLE 'teams' (
-                         'id' bigint(20) NOT NULL,
-                         'name' varchar(255) NOT NULL,
-                         'sport_id' int(11) NOT NULL,
-                         'captain_id' int(11) DEFAULT NULL,
-                         'city' varchar(255) DEFAULT NULL,
-                         'foundation_date' date DEFAULT NULL
+CREATE TABLE `teams` (
+                         `id` bigint(20) NOT NULL,
+                         `name` varchar(255) NOT NULL,
+                         `sport_id` int(11) NOT NULL,
+                         `captain_id` int(11) DEFAULT NULL,
+                         `city` varchar(255) DEFAULT NULL,
+                         `foundation_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -64,57 +70,61 @@ CREATE TABLE 'teams' (
 --
 
 --
--- Indices de la tabla 'players'
+-- Indices de la tabla `players`
 --
-ALTER TABLE 'players'
-    ADD PRIMARY KEY ('id'),
-  ADD KEY 'idx_team' ('team_id');
+ALTER TABLE `players`
+    ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_team` (`team_id`);
 
 --
--- Indices de la tabla 'sports'
+-- Indices de la tabla `sports`
 --
-ALTER TABLE 'sports'
-    ADD PRIMARY KEY ('id');
+ALTER TABLE `sports`
+    ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla 'teams'
+-- Indices de la tabla `teams`
 --
-ALTER TABLE 'teams'
-    ADD PRIMARY KEY ('id'),
-  ADD KEY 'FK_TeamsSports' ('sport_id'),
-  ADD KEY 'FK_CaptainPlayer' ('captain_id');
+ALTER TABLE `teams`
+    ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_TeamsSports` (`sport_id`),
+  ADD KEY `FK_CaptainPlayer` (`captain_id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla 'players'
+-- AUTO_INCREMENT de la tabla `players`
 --
-ALTER TABLE 'players'
-    MODIFY 'id' int(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `players`
+    MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla 'sports'
+-- AUTO_INCREMENT de la tabla `sports`
 --
-ALTER TABLE 'sports'
-    MODIFY 'id' int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `sports`
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT de la tabla 'teams'
+-- AUTO_INCREMENT de la tabla `teams`
 --
-ALTER TABLE 'teams'
-    MODIFY 'id' bigint(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `teams`
+    MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla 'teams'
+-- Filtros para la tabla `teams`
 --
-ALTER TABLE 'teams'
-    ADD CONSTRAINT 'FK_CaptainPlayer' FOREIGN KEY ('captain_id') REFERENCES 'players' ('id'),
-  ADD CONSTRAINT 'FK_TeamsSports' FOREIGN KEY ('sport_id') REFERENCES 'sports' ('id'),
-  ADD CONSTRAINT 'teams_ibfk_1' FOREIGN KEY ('sport_id') REFERENCES 'sports' ('id');
+ALTER TABLE `teams`
+    ADD CONSTRAINT `FK_CaptainPlayer` FOREIGN KEY (`captain_id`) REFERENCES `players` (`id`),
+  ADD CONSTRAINT `FK_TeamsSports` FOREIGN KEY (`sport_id`) REFERENCES `sports` (`id`),
+  ADD CONSTRAINT `teams_ibfk_1` FOREIGN KEY (`sport_id`) REFERENCES `sports` (`id`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
